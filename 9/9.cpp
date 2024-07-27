d313a915feda44ddc36fd215ae851b0452fb312f
K1r177 <K1r177@yandex.ru>
2024-07-27 13:37:17 +0300

    first commit, arrays 9 task

---
+++ 9/9.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+    
+    string str = "-1 2 3 -1 -2";
+    vector <int> arr;
+    istringstream iss(str);
+    int number;
+    int coNum = 0;
+    
+    while (iss >> number) {
+        arr.push_back(number);
+    }
+    
+    for(int i = 1; i < arr.size(); i++){
+        if(arr[i - 1] > 0 && arr[i] > 0){
+            cout << arr[i - 1] << " " << arr[i] << " ";
+             coNum = coNum + 2;
+        }
+        else if(arr[i - 1] < 0 && arr[i] < 0){
+            cout << arr[i - 1] << " " << arr[i] << " ";
+            coNum = coNum + 2;
+        }
+        
+        if(coNum > 1){
+            break;
+        }
+        
+    }
+
+    cout << endl;
+
+    system("pause");
+    
+    return 0;
+}
